1adadb629d271e7e42defefccfe7358eb2a4c9f1
encrypticus <encrypticus@gmail.com>
2020-02-11 20:08:00 +0600

    Преобразовал DataTable в компонент-класс.

--- src/containers/data-table/data-table.js
+++ src/containers/data-table/data-table.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import withDataService from '$c/hoc';
 
-const DataTable = (props) => {
-  console.log(props);
-  return (
-    <table className='data-table'>
-      <tbody>
+class DataTable extends React.Component {
+
+  render() {
+    return (
+      <table className='data-table'>
+        <tbody>
         <tr>
           <th>Номер ТП</th>
           <th>Номер счетчика</th>
           <th>Показания</th>
           <th>Дата</th>
         </tr>
-      </tbody>
-    </table>
-  );
-};
+        </tbody>
+      </table>
+    );
+  }
+}
 
 export default withDataService()(DataTable);
