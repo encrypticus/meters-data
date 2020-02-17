757ef89db168ad2f468486d8d1dbf0c2a326aa3c
encrypticus <encrypticus@gmail.com>
2020-02-17 12:25:34 +0600

    Добавление модального окна подтверждения удаления записи
    src/redux/reducers/reducer.js: добавил новую часть redux store.

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import metersData from './meters-data';
 import stations from './stations';
+import modal from './modal';
 
 export default combineReducers({
   metersData,
-  stations
+  stations,
+  modal
 });
